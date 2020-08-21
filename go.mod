e23c4c7bb7f1b05bbb3963080039f371cc72be74
rain <shuyeyaojing@foxmail.com>
2020-08-21 15:48:31 +0800

    add snowflake way

---
+++ go.mod
@@ -0,0 +1,5 @@
+module util
+
+go 1.14
+
+require github.com/bwmarrin/snowflake v0.3.0
--- snowflake/snowflake.go
+++ snowflake/snowflake.go
@@ -5,14 +5,39 @@ import (
 	"github.com/bwmarrin/snowflake"
 )
 
-func SnowflakeId() string {
+func GetSnowflakeId() snowflake.ID {
 	// Create a new Node with a Node number of 1
 	node, err := snowflake.NewNode(1)
 	if err != nil {
 		fmt.Println(err)
-		return ""
+		return 0
 	}
-	// Generate a snowflake ID.
-	id := node.Generate()
-	return id.Base58()
+	return node.Generate()
+}
+func SnowflakeBase2() string {
+	return GetSnowflakeId().Base2()
+}
+func SnowflakeBase32() string {
+	return GetSnowflakeId().Base32()
+}
+func SnowflakeBase36() string {
+	return GetSnowflakeId().Base36()
+}
+func SnowflakeBase58() string {
+	return GetSnowflakeId().Base58()
+}
+func SnowflakeBase64() string {
+	return GetSnowflakeId().Base64()
+}
+func SnowflakeString() string {
+	return GetSnowflakeId().String()
+}
+func SnowflakeBytes() []byte {
+	return GetSnowflakeId().Bytes()
+}
+func SnowflakeInt64() int64 {
+	return GetSnowflakeId().Int64()
+}
+func SnowflakeIntBytes() [8]byte {
+	return GetSnowflakeId().IntBytes()
 }
